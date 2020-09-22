04ee1bc3070a0119defa6759a8eb3df2565ea532
mfrachet <marvin@gatsbyjs.com>
2020-09-22 14:00:13 +0200

    Making SSE a bit better in term of API

--- packages/use-server-sent-events/src/provider.tsx
+++ packages/use-server-sent-events/src/provider.tsx
@@ -4,14 +4,17 @@ import { SSEContext } from "./context";
 export interface ISSEProviderProps {
   url: string;
   opts?: EventSourceInit;
+  onOpen?: (ev: Event) => void;
 }
 
 export const SSEProvider: React.FC<ISSEProviderProps> = ({
   url,
   opts,
   children,
+  onOpen,
 }) => {
   const eventSourceRef = React.useRef<EventSource>();
+  const onOpenRef = React.useRef<(ev: Event) => void>();
 
   if (!window) {
     return <>{children}</>;
@@ -20,6 +23,18 @@ export const SSEProvider: React.FC<ISSEProviderProps> = ({
   if (!eventSourceRef.current) {
     eventSourceRef.current = new EventSource(url, opts);
   }
+
+  onOpenRef.current = onOpen;
+
+  React.useEffect(() => {
+    if (onOpenRef?.current) {
+      eventSourceRef.current.onopen = onOpenRef.current;
+    }
+
+    return () => {
+      eventSourceRef?.current?.close();
+    };
+  }, []);
 
   return (
     <SSEContext.Provider value={eventSourceRef.current}>
